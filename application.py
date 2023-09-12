75a50fbd60619c0afc69e4fe416f034927b777ce
MarjanSadat <marjanmousavi@ymail.com>
2023-09-12 22:35:19 +0400

    create ui

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
-
+car = pd.read_csv('model/Cleaned_Car.csv')
 @app.route('/')
 def index():
-    return 'hello'
+    companies = sorted(car['company'].unique())
+    car_models = sorted(car['name'].unique())
+    year = sorted(car['year'].unique(), reverse=True)
+    fuel_type = car['fuel_type'].unique()
+    return render_template('index.html',companies=companies, car_models=car_models,years=year,fuel_type=fuel_type)
 
 if __name__ == '__main__':
     app.run(debug=True)
