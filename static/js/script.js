036fa904739fb5c4bc403029e061bd2aac4e3aa5
MarjanSadat <marjanmousavi@ymail.com>
2023-09-13 15:16:55 +0400

    final

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+function load_car_models(this.id, car_model_name){
+    console.log('9999999999999999999999999')
+}
+
+function form_handler(event){
+    event.preventDefault();
+}
+
+function send_data(){
+    console.log('000000000000000000000000000000000')
+    document.querySelector('form').addEventListener('submit',form_handler);
+}
